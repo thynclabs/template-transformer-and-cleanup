b7eef7d1f1cb7ff15ea3a9d65ab4d8ba12b3a3ca
Kumar Kisalay <krisalay@outlook.com>
2024-06-25 04:54:43 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,8 @@ set -e
 export LANG=C.UTF-8
 
 # Replace "npm-template" with the new repository name in all files
-find /github/workspace -type f -exec sed -i 's/npm-template/script/g' {} +
-# sed -i 's/npm-template/script/g' /github/workspace/package.json
-
+# find /github/workspace -type f -exec sed -i 's/npm-template/script/g' {} +
+echo "hello" >> test.txt
 ls
 cat package.json
 cat .github/ISSUE_TEMPLATE/bug_report.yaml
@@ -19,10 +18,10 @@ cat .github/ISSUE_TEMPLATE/bug_report.yaml
 #fi
 
 # Commit changes
-#git config --local user.email "actions@github.com"
-#git config --local user.name "GitHub Actions"
-#git add .
-#git commit -m "Replace 'my-template' with '${INPUT_TEXT}' and delete .github folder"
-#git push origin main
+git config --local user.email "actions@github.com"
+git config --local user.name "GitHub Actions"
+git add .
+git commit -m "Replace 'my-template' with '${INPUT_TEXT}' and delete .github folder"
+git push
 
 echo "Action completed successfully!"
